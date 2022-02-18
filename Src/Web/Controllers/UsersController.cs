83dcdcff664b6f8b53ef38176e44882ec1e7658d
ZaqueuCavalcante <zaqueudovale@gmail.com>
2022-02-17 23:04:27 -0300

    Add cookie

--- Src/Web/Controllers/UsersController.cs
+++ Src/Web/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Web.Models;
 using Web.Services;
 
@@ -38,7 +42,27 @@ public class UsersController : Controller
     {
         var response = await _authService.Login(dto);
 
-        return View();
+        var token = new JwtSecurityTokenHandler().ReadToken(response.AccessToken) as JwtSecurityToken;
+
+        var claims = new List<Claim>();
+        claims.Add(new Claim("JWT", response.AccessToken));
+        claims.AddRange(token.Claims);
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+            IsPersistent = true
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties  
+        );
+
+        return RedirectToAction("Index", "Home");
     }
 
     [HttpPost("logout")]
